c799db34097a6a616539156ed85a099c9fa627d2
Pattarapon <pattarapon.kitcharoen@gmail.com>
2026-05-12 08:34:08 +0700

    Fix Icon

--- app/+html.tsx
+++ app/+html.tsx
@@ -4,6 +4,7 @@ export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <html lang="th">
       <head>
+        <link rel="manifest" href="/site.webmanifest" />
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
